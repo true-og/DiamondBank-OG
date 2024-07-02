09a14a7c85a068d2c6430dce293edf4c8637e4e1
Sven <68018116+SKBotNL@users.noreply.github.com>
2024-07-02 21:56:06 +0200

    Fix indentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.23"
-	id("io.github.goooler.shadow") version "8.1.8"
+    id("io.github.goooler.shadow") version "8.1.8"
     id("maven-publish")
     id("eclipse")
 }
