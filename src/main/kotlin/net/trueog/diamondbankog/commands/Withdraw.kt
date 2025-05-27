688dec0dba26e18edcaa55e6df95a02318ffbcf5
Sven <skbotnl@disroot.org>
2025-05-27 23:37:57 +0200

    Remove debug printlns

--- src/main/kotlin/net/trueog/diamondbankog/commands/Withdraw.kt
+++ src/main/kotlin/net/trueog/diamondbankog/commands/Withdraw.kt
@@ -105,15 +105,7 @@ class Withdraw : CommandExecutor {
                 Shard.namespacedKey) }
                 .sumOf { 64 - it.amount }
 
-            println(diamondAmount)
-            println(leftOverSpaceDiamonds)
-            println(diamondAmount > (emptySlots * 64 + leftOverSpaceDiamonds))
-            println(shardAmount)
-            println(leftOverSpaceShards)
-
             val emptySlotsAfterDiamonds = if (emptySlots != 0) emptySlots - ceil(diamondAmount / 64.0) else 0.0
-            println(shardAmount > (emptySlotsAfterDiamonds * 64 + leftOverSpaceShards))
-            println(emptySlotsAfterDiamonds * 64 + leftOverSpaceShards)
 
             if (diamondAmount > (emptySlots * 64 + leftOverSpaceDiamonds) || shardAmount > (emptySlotsAfterDiamonds * 64 + leftOverSpaceShards)) {
                 val diamonds = String.format("%.1f", floor((shards / 9.0) * 10) / 10.0)
