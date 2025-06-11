143cfd39987745ba78a93ffd632bd06d94f0d341
Sven <skbotnl@disroot.org>
2025-06-11 22:23:39 +0200

    Improve economy-related item usage prevention

--- src/main/kotlin/net/trueog/diamondbankog/Events.kt
+++ src/main/kotlin/net/trueog/diamondbankog/Events.kt
@@ -13,13 +13,14 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.inventory.PrepareItemCraftEvent
 import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerInteractEntityEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.scheduler.BukkitRunnable
@@ -215,15 +216,68 @@ internal class Events : Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    fun onBlockPlace(event: BlockPlaceEvent) {
-        val worldName = event.player.world.name
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        val player = event.player
+        val worldName = player.world.name
         if (worldName != "world" && worldName != "world_nether" && worldName != "world_the_end") return
 
-        if (event.block.type != Material.DIAMOND_BLOCK && event.block.type != Material.SHULKER_BOX) return
+        val clickedBlock = event.clickedBlock
+        if (clickedBlock == null) return
+        if (clickedBlock.type == Material.AIR) return
+
+        if (event.action.isLeftClick) {
+            if (clickedBlock.type != Material.DIAMOND_BLOCK && clickedBlock.type != Material.SHULKER_BOX) {
+                return
+            }
+        } else {
+            val itemStack = event.item
+            if (itemStack == null) return
+            val itemType = itemStack.type
+
+            if (itemType != Material.DIAMOND && itemType != Material.DIAMOND_BLOCK && itemType != Material.SHULKER_BOX && !(itemType == Material.PRISMARINE_SHARD && itemStack.persistentDataContainer.has(
+                    Shard.namespacedKey
+                ))
+            ) {
+                return
+            }
+        }
 
         if (DiamondBankOG.economyDisabled) {
             event.isCancelled = true
-            event.player.sendMessage(DiamondBankOG.mm.deserialize("${Config.prefix}<reset>: <red>You cannot place any economy-related items while the economy is disabled."))
+            event.player.sendMessage(DiamondBankOG.mm.deserialize("${Config.prefix}<reset>: <red>You cannot interact with any economy-related items while the economy is disabled."))
+            return
+        }
+
+        if (DiamondBankOG.transactionLock.isLocked(event.player.uniqueId)) {
+            event.isCancelled = true
+            return
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onPlayerInteractEntity(event: PlayerInteractEntityEvent) {
+        val player = event.player
+        val worldName = player.world.name
+        if (worldName != "world" && worldName != "world_nether" && worldName != "world_the_end") return
+
+        val itemStackInMainHand = player.inventory.itemInMainHand
+        val itemTypeInMainHand = itemStackInMainHand.type
+
+        val itemStackInOffHand = player.inventory.itemInOffHand
+        val itemTypeInOffHand = itemStackInOffHand.type
+
+        if ((itemTypeInMainHand != Material.DIAMOND && itemTypeInMainHand != Material.DIAMOND_BLOCK && itemTypeInMainHand != Material.SHULKER_BOX && !(itemTypeInMainHand == Material.PRISMARINE_SHARD && itemStackInMainHand.persistentDataContainer.has(
+                Shard.namespacedKey
+            )) && (itemTypeInOffHand != Material.DIAMOND && itemTypeInOffHand != Material.DIAMOND_BLOCK && itemTypeInOffHand != Material.SHULKER_BOX && !(itemTypeInOffHand == Material.PRISMARINE_SHARD && itemStackInOffHand.persistentDataContainer.has(
+                Shard.namespacedKey
+            ))))
+        ) {
+            return
+        }
+
+        if (DiamondBankOG.economyDisabled) {
+            event.isCancelled = true
+            event.player.sendMessage(DiamondBankOG.mm.deserialize("${Config.prefix}<reset>: <red>You cannot interact with any economy-related items while the economy is disabled."))
             return
         }
 
