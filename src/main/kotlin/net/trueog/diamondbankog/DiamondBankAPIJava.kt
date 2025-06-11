51f1a6e59cdaa506bddef86a3821f4f6af268fb7
Sven <skbotnl@disroot.org>
2025-06-11 21:43:38 +0200

    Move DiamondBankException to its own class

--- src/main/kotlin/net/trueog/diamondbankog/DiamondBankAPIJava.kt
+++ src/main/kotlin/net/trueog/diamondbankog/DiamondBankAPIJava.kt
@@ -12,38 +12,6 @@ import java.util.concurrent.CompletableFuture
 
 @OptIn(DelicateCoroutinesApi::class)
 class DiamondBankAPIJava(private var postgreSQL: PostgreSQL) {
-    sealed class DiamondBankException(message: String) : Exception(message) {
-        object TransactionsLockedException : DiamondBankException("Transactions for player are locked") {
-            @Suppress("unused")
-            private fun readResolve(): Any = TransactionsLockedException
-        }
-
-        object EconomyDisabledException : DiamondBankException("Economy is disabled") {
-            @Suppress("unused")
-            private fun readResolve(): Any = EconomyDisabledException
-        }
-
-        object InvalidPlayerException : DiamondBankException("Invalid player") {
-            @Suppress("unused")
-            private fun readResolve(): Any = InvalidPlayerException
-        }
-
-        object PayerNotOnlineException : DiamondBankException("Payer is not online") {
-            @Suppress("unused")
-            private fun readResolve(): Any = PayerNotOnlineException
-        }
-
-        object PlayerNotOnlineException : DiamondBankException("Player is not online") {
-            @Suppress("unused")
-            private fun readResolve(): Any = PlayerNotOnlineException
-        }
-
-        object OtherException : DiamondBankException("Other exception") {
-            @Suppress("unused")
-            private fun readResolve(): Any = OtherException
-        }
-    }
-
     /**
      * WARNING: blocking, if the player has a transaction lock applied this function will wait until its released
      * @param transactionReason the reason for this transaction for in the transaction log
