a81ff8de57aac94fb4fd8ae04711c2c3f82dd064
Sven <skbotnl@disroot.org>
2026-04-04 13:22:23 +0200

    Switch to a thread-safe list in EventManager

--- src/main/kotlin/net/trueog/diamondbankog/EventManager.kt
+++ src/main/kotlin/net/trueog/diamondbankog/EventManager.kt
@@ -1,9 +1,10 @@
 package net.trueog.diamondbankog
 
 import java.util.*
+import java.util.concurrent.CopyOnWriteArrayList
 
 internal class EventManager {
-    private var listeners = listOf<PlayerBalanceChangedListener>()
+    private var listeners = CopyOnWriteArrayList<PlayerBalanceChangedListener>()
 
     fun sendUpdate(uuid: UUID, playerShards: PostgreSQL.PlayerShards) {
         listeners.forEach { it.onUpdate(PlayerBalanceChangedEvent(uuid, playerShards)) }
