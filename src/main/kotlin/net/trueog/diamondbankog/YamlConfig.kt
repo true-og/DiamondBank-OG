e7cb147af849c62c3be7f4b8f6cd068faed00c54
Sven <skbotnl@disroot.org>
2026-04-05 19:00:50 +0200

    Remove now unused Postgres table and log limit config options

--- src/main/kotlin/net/trueog/diamondbankog/YamlConfig.kt
+++ src/main/kotlin/net/trueog/diamondbankog/YamlConfig.kt
@@ -1,7 +1,6 @@
 package net.trueog.diamondbankog
 
 import java.io.File
-import kotlin.properties.Delegates
 import net.trueog.diamondbankog.DiamondBankOG.Companion.plugin
 import org.bukkit.configuration.file.YamlConfiguration
 
@@ -10,9 +9,6 @@ internal class YamlConfig private constructor() : Config {
     override lateinit var postgresUrl: String
     override lateinit var postgresUser: String
     override lateinit var postgresPassword: String
-    override lateinit var postgresTable: String
-    override lateinit var postgresLogTable: String
-    override var postgresLogLimit by Delegates.notNull<Int>()
     override lateinit var redisUrl: String
 
     companion object : ConfigFactory {
@@ -47,31 +43,6 @@ internal class YamlConfig private constructor() : Config {
                 config.postgresPassword = yamlConfig.get("postgresPassword") as String
             } catch (_: Exception) {
                 plugin.logger.severe("Failed to parse config option \"postgresPassword\" as a string")
-                return null
-            }
-
-            try {
-                config.postgresTable = yamlConfig.get("postgresTable") as String
-            } catch (_: Exception) {
-                plugin.logger.severe("Failed to parse config option \"postgresTable\" as a string")
-                return null
-            }
-
-            try {
-                config.postgresLogTable = yamlConfig.get("postgresLogTable") as String
-            } catch (_: Exception) {
-                plugin.logger.severe("Failed to parse config option \"postgresLogTable\" as a string")
-                return null
-            }
-
-            try {
-                config.postgresLogLimit = yamlConfig.get("postgresLogLimit") as Int
-                if (config.postgresLogLimit < 1) {
-                    plugin.logger.severe("\"postgresLogLimit\" cannot be less than 1")
-                    return null
-                }
-            } catch (_: Exception) {
-                plugin.logger.severe("Failed to parse config option \"postgresLogLimit\" as an integer")
                 return null
             }
 
