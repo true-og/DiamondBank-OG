9f5153a043aa60651b8f1a7e2619bbae485b96f2
Sven <skbotnl@disroot.org>
2026-07-28 13:00:39 +0200

    Prevent deadlock in listener

--- src/main/kotlin/net/trueog/diamondbankog/api/EventManager.kt
+++ src/main/kotlin/net/trueog/diamondbankog/api/EventManager.kt
@@ -2,13 +2,25 @@ package net.trueog.diamondbankog.api
 
 import java.util.*
 import java.util.concurrent.CopyOnWriteArrayList
+import java.util.logging.Level
+import kotlinx.coroutines.launch
+import net.trueog.diamondbankog.DiamondBankOG.Companion.plugin
+import net.trueog.diamondbankog.DiamondBankOG.Companion.scope
 import net.trueog.diamondbankog.balance.shard.PlayerShards
 
 internal class EventManager {
     private var listeners = CopyOnWriteArrayList<PlayerBalanceChangedListener>()
 
     fun sendUpdate(uuid: UUID, playerShards: PlayerShards) {
-        listeners.forEach { it.onUpdate(PlayerBalanceChangedEvent(uuid, playerShards)) }
+        scope.launch { // Launch new coroutine to dispatch listeners to avoid TransactionLock deadlock
+            listeners.forEach {
+                try {
+                    it.onUpdate(PlayerBalanceChangedEvent(uuid, playerShards))
+                } catch (e: Exception) {
+                    plugin.logger.log(Level.SEVERE, "PlayerBalanceChangedEvent listener threw", e)
+                }
+            }
+        }
     }
 
     fun register(listener: PlayerBalanceChangedListener) {
